6446d36a3260fb262b487ffb375d13348c0aa97b
jie.yang <jie.yang@witontek.com>
2017-04-05 17:50:25 +0800

    update

--- theme/Project/hasyqyh/qyhMng/js/login.js
+++ theme/Project/hasyqyh/qyhMng/js/login.js
@@ -3,12 +3,12 @@ function resizeImg(){
   var w = $(window).width(),
       h = $(window).height();
     if(w>=1349){
-    	$('.authcenter').height(h);
+    	//$('.authcenter').height(h);
     	$('.authcenter-background').height(h);
 		$('#bgImg').width(w);
 		$('#bgImg').height(w/1.6);
     }else{
-    	$('.authcenter').height(643);
+    	//$('.authcenter').height(643);
     	$('.authcenter-background').height(643);
 		$('#bgImg').width(1349);
 		$('#bgImg').height(1349/1.6);
