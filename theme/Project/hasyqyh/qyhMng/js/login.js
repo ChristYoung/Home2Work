7bdf47237831f24c0e6693550791e22c57daf2b7
jie.yang <jie.yang@witontek.com>
2017-04-05 14:01:46 +0800

    update

---
+++ theme/Project/hasyqyh/qyhMng/js/login.js
@@ -0,0 +1,20 @@
+//适配背景图
+function resizeImg(){
+  var w = $(window).width(),
+      h = $(window).height();
+    if(w>=1349){
+    	$('.authcenter-background').height(h);
+		$('#bgImg').width(w);
+		$('#bgImg').height(w/1.6);
+    }else{
+    	$('.authcenter-background').height(643);
+		$('#bgImg').width(1349);
+		$('#bgImg').height(1349/1.6);
+    }
+};
+$(window).resize(function(){
+	 resizeImg();
+});
+$(document).ready(function(){
+	resizeImg();
+});
