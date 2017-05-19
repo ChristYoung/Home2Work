261657187c9df6b8523bbda43c766fc019f55e08
jie.yang <jie.yang@witontek.com>
2017-05-19 14:23:59 +0800

    delete something repeat

--- myLearning/Project/jsLearn/VueJs/vueMk/vuemk/src/store.js
+++
@@ -1,9 +0,0 @@
-var STORAGE_KEY = "todos-vuejs";
-export default{
-	fetch(){
-		return JSON.parse(window.localStorage.getItem(STORAGE_KEY) || '[]')
-	},
-	save(item){
-		window.localStorage.setItem(STORAGE_KEY,JSON.stringify(item));
-	}
-}
