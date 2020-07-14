84325f4b4ee7d91c0622a9d3528969ffd76cbbc0
yangjie <jie.yang@witontek.com>
2020-07-14 11:40:42 +0800

    update--新增子路由调试

--- ngBook8/src/app/child-routes/child-routes-routing.module.ts
+++ ngBook8/src/app/child-routes/child-routes-routing.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PathLibrary } from 'app/public/path-library';
 import { ChildRoutesBoxComponent } from './child-routes-box/child-routes-box.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
 
 const routes: Routes = [
     {
-        path: PathLibrary.childRoutesBox, component: ChildRoutesBoxComponent,
+        path: PathLibrary.childRoutesBox,
+        component: ChildRoutesBoxComponent,
+        children: [
+            {
+                path: 'childA',
+                component: ChildAComponent,
+            },
+            {
+                path: 'childB',
+                component: ChildBComponent,
+            }
+        ]
     }
 ];
 
 @NgModule({
     imports: [
+        RouterModule.forChild(routes),
     ],
     exports: [RouterModule]
 })
--- ngBook8/src/app/child-routes/child-routes.module.ts
+++ ngBook8/src/app/child-routes/child-routes.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChildRoutesBoxComponent } from './child-routes-box/child-routes-box.component';
 import { ChildRoutesRoutingModule } from './child-routes-routing.module';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
 
 @NgModule({
-  declarations: [ChildRoutesBoxComponent],
+  declarations: [ChildRoutesBoxComponent, ChildAComponent, ChildBComponent],
   imports: [
     CommonModule,
     ChildRoutesRoutingModule,
