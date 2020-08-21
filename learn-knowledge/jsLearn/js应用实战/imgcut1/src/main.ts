7910fd8f4434a40e2cdceb0d8ab2781401cd5b8e
ChristYoung <858851768@qq.com>
2020-08-21 16:55:31 +0800

    update--ts版本下的图片预览imgcut1

--- learn-knowledge/jsLearn/js应用实战/imgcut1/src/main.ts
+++ learn-knowledge/jsLearn/js应用实战/imgcut1/src/main.ts
@@ -1,5 +1,13 @@
-function helloWorld(): void {
-    alert('hello world~~~');
+// 1. 获取文件并读取文件
+const fileInputChange = () => {
+    const fileInput$ = document.getElementById('fileInput') as HTMLInputElement;
+    const img$ = document.getElementById('imgContainer') as HTMLImageElement;
+    fileInput$.onchange = (e: Event) => {
+        const file = fileInput$.files[0];
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = (e) => img$.src = e.target.result as string;
+    }
 }
+fileInputChange();
 
-helloWorld();
