454fa924217793926b8b5e4c769e9a99a1d42e1e
yangjie <jie.yang@witontek.com>
2020-08-22 15:50:04 +0800

    update--format

--- learn-knowledge/jsLearn/js应用实战/imgcut1/src/main.ts
+++ learn-knowledge/jsLearn/js应用实战/imgcut1/src/main.ts
@@ -2,12 +2,17 @@
 const fileInputChange = () => {
     const fileInput$ = document.getElementById('fileInput') as HTMLInputElement;
     const img$ = document.getElementById('imgContainer') as HTMLImageElement;
-    fileInput$.onchange = (e: Event) => {
-        const file = fileInput$.files[0];
+    fileInput$.addEventListener('change', (e) => {
+        console.log('this', this);  // 箭头函数内部没有this, 箭头函数中的this是定义时绑定, 也就是this是继承父执行上下文
+        const file = (e.target as HTMLInputElement).files[0];
         const fileReader = new FileReader();
         fileReader.readAsDataURL(file);
         fileReader.onload = (e) => img$.src = e.target.result as string;
-    }
+    });
 }
+
+// 2. 获取裁剪坐标
+const handleMouseDown = (e) => 
+
 fileInputChange();
 
