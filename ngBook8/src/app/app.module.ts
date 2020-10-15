3b808c77d4cfa8c025310055e5751f0f1ea083e4
ChristYoung <858851768@qq.com>
2020-10-15 16:21:28 +0800

    🆕: ng-zorro

--- ngBook8/src/app/app.module.ts
+++ ngBook8/src/app/app.module.ts
@@ -1,10 +1,17 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppRoutingModule} from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
-import {AppComponent} from './app.component';
-import {DemoListModule} from './demo-list/demo-list.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { DemoListModule } from './demo-list/demo-list.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -15,8 +22,11 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     DemoListModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent],
 })
 export class AppModule {
